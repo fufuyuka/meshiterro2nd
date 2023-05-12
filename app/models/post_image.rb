7f46a997199f2d3a30e5cbd767ec0fbb7e959566
fufuyuka <121597200+fufuyuka@users.noreply.github.com>
2023-05-12 11:17:41 +0000

    [add]post_image(投稿、詳細、一覧)

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,4 +2,14 @@ class PostImage < ApplicationRecord
   #ActiveStorageでimageカラムとして画像を持たせる
   has_one_attached :image
   belongs_to :user
+  
+  #画像未保存の場合のデフォルト画像の差し込み
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
+  
 end
